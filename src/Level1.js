a64e33f5902b88fc516b6f2120d1be7f0e15320c
Jonny <jorfn@hotmail.co.uk>
2018-04-14 17:40:38 +0100

    test3.11

--- src/Level1.js
+++ src/Level1.js
@@ -118,7 +118,7 @@ Game.Level1.prototype = {
       game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
       game.scale.pageAlignHorizontally = true;
       game.scale.pageAlignVertically = true;
-      game.scale.forceOrientation(true, true);
+      game.scale.forceOrientation(false, false);
       game.scale.updateLayout(true);
       game.scale.refresh();
       game.input.onDown.add(function() {
